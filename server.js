a4961fcdedf2545cdd84d4ada99ba83d0dfd6de6
Andrew <shifted6@gmail.com>
2020-02-18 13:33:36 -0800

    Added weather request response function.

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.get('/location', (request, response)=>{
     let cityQuery = request.query.city;
     let geoData = require('./data/geo.json');
     let newCity = new City(cityQuery, geoData[0]);
-
+    console.log(request);
+    console.log(newCity);
     response.send(newCity);
     }
     catch (err){
@@ -29,6 +30,25 @@ function City(city, obj){
     this.formatted_query = obj.display_name;
     this.latitude = obj.lat;
     this.longitude = obj.lon;
+}
+
+app.get('/weather', (request,response)=>{
+    let weather = [];
+    let latQuery = request.query.latitude;
+    let lonQuery = request.query.longitude;
+    let weatherData = require('./data/darksky.json');
+    let dailyWeatherData = weatherData.daily.data;
+    dailyWeatherData.forEach(day => {
+        let newWeather = new Weather(day);
+        console.log(newWeather);
+        weather.push(newWeather);
+    })
+    response.send(weather);
+})
+
+function Weather(obj){
+    this.forecast = obj.summary;
+    this.time = new Date(obj.time * 1000).toString().slice(0,15);
 }
 
 app.listen(PORT, ()=>{
