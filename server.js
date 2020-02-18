5f606509ca84ae1e5039dfe6af81e0956db71544
Andrew <shifted6@gmail.com>
2020-02-18 11:35:23 -0800

    Added request response for front-end from geo data.

--- server.js
+++ server.js
@@ -11,11 +11,25 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get('/locations', (request, response)=>{
+    try{
     let cityQuery = request.query.city;
     let geoData = require('./data/geo.json');
-    console.log(request);
-    // response.send(location);
+    let newCity = new City(cityQuery, geoData[0]);
+
+    response.send(newCity);
+    }
+    catch (err){
+        console.log(request);
+        console.log(err);
+    }
 })
+
+function City(city, obj){
+    this.search_query = city;
+    this.formatted_query = obj.display_name;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
+}
 
 app.listen(PORT, ()=>{
     console.log(`listening on ${PORT}`);
